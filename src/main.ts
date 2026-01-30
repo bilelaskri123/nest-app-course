cd71471e9aecdf3de8fb74d00d44eb267391bd90
askribilel123 <askribilel09@gmail.com>
2026-01-30 14:19:56 +0100

    add and config swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerInterceptor } from './utils/interceptors/logger.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,11 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
   });
+
+  const swagger = new DocumentBuilder().setVersion('1.0').build();
+  const documentation = SwaggerModule.createDocument(app, swagger);
+  // http://localhost:5000/swagger
+  SwaggerModule.setup('swagger', app, documentation);
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
