3c4b4380a8de4d41ed2c53fc3c1b1277c0b07680
askribilel123 <askribilel09@gmail.com>
2026-01-31 17:11:44 +0100

    apply helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerInterceptor } from './utils/interceptors/logger.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,10 +15,15 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new LoggerInterceptor());
+  // Apply Middlewares
+  app.use(helmet());
+
+  // Cors Policy
   app.enableCors({
     origin: 'http://localhost:3000',
   });
 
+  // swagger
   const swagger = new DocumentBuilder()
     .setTitle('Ecomerce Application with NestJS - AppAPI')
     .setDescription('Your API Description')
@@ -29,6 +35,8 @@ async function bootstrap() {
   const documentation = SwaggerModule.createDocument(app, swagger);
   // http://localhost:5000/swagger
   SwaggerModule.setup('swagger', app, documentation);
+
+  // Running the app
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
